e1ca7f442ab61a3cb8292dba9e375d69737b6a3b
Stefan Hagen <stefan@hagen.link>
2021-02-14 18:36:11 +0100

    Added positive test case for known tree to functional tests

--- tests/test_license_screening.py
+++ tests/test_license_screening.py
@@ -9,3 +9,7 @@ import license_screening.license_screening as lis
 
 def test_parse_ok_empty_string():
     assert lis.parse('') is NotImplemented
+
+
+def test_parse_ok_known_tree():
+    assert lis.main(["tests/data"]) == 0
