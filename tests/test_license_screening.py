a2d4ba52785b0e52e0ba0140e269b01203bb7415
Stefan Hagen <stefan@hagen.link>
2021-02-14 18:01:24 +0100

    Added functional test suite

---
+++ tests/test_license_screening.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,unused-import,reimported
+import pytest  # type: ignore
+
+import license_screening.license_screening as lis
+
+
+def test_parse_ok_empty_string():
+    assert lis.parse('') is NotImplemented
